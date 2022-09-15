55c4e6e8aec5105106c3747768892a350a9c6ea9
fehporto97 <feh.porto97@gmail.com>
2022-09-15 20:23:15 -0300

    criacao e alteracao dos conteudos do projeto

--- frontend/src/pages/site/Instituicoes.jsx
+++ frontend/src/pages/site/Instituicoes.jsx
@@ -1,54 +1,52 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { Container, NavLink, Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import insti_img from '../../assets/img/instituicoes.png';
-import principal_img from '../../assets/styles/Instituicao.css';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Container, NavLink, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import insti_img from "../../assets/img/instituicoes.png";
+import principal_img from "../../assets/styles/Instituicao.css";
+import Footer from "./components/Footer";
 
 export default function Instituicoes() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-      fetch('http://localhost:3000/static/ongs.json')
-        .then((response) => response.json())
-        .then(setData);
-  },[])
-
-
+    fetch("http://localhost:3000/static/ongs.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
 
   return (
     <div>
-    <Container className="mt-3">
-      <img src={insti_img} className="principal_img"/>
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Foto</th>
-            <th>Nome</th>
-            <th>Descrição</th>
-            <th>Site</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, id) => (
-            <tr key={id}>
-              <td>{item.id}</td>
-              <td>
-                <img src={item.image} width={100}/>
-              </td>
-              <td>{item.name}</td>
-              <td>{item.desc}</td>
-              <td>
-                <NavLink link={data.site}>Site</NavLink>
-              </td>
+      <Container className="mt-3">
+        <img src={insti_img} className="principal_img" />
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Foto</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Site</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Footer />
-    </Container>
+          </thead>
+          <tbody>
+            {data.map((item, id) => (
+              <tr key={id}>
+                <td>{item.id}</td>
+                <td>
+                  <img src={item.image} width={100} />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.desc}</td>
+                <td>
+                  <NavLink link={data.site}>Site</NavLink>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Footer />
+      </Container>
     </div>
-  )
+  );
 }
--- frontend/src/pages/site/components/Navibar.jsx
+++ frontend/src/pages/site/components/Navibar.jsx
@@ -7,9 +7,8 @@ function Navibar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#">Quem somos</Nav.Link>
             <Nav.Link href="/mapa">Mapa</Nav.Link>
-            <Nav.Link href="#">Contato</Nav.Link>
+            <Nav.Link href="/quemsomos">Quem somos</Nav.Link>
             <Nav.Link href="/instituicoes">Instituicoes</Nav.Link>
             <Nav.Link href="/login" className="Login-nav">
               Login
