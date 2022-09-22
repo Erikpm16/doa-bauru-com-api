3e9ef5862ecd96f67bbd331efae04b24aca6d87d
Erikpm16 <erikpm16@hotmail.com>
2022-09-21 22:00:00 -0300

    ajuste api do backend para testes, otimização da pagina ONG-detalhes

--- backend/src/models.test.js
+++ backend/src/models.test.js
@@ -15,6 +15,16 @@ describe("models", () =>{
         expect(user).toBeDefined();
         expect(user.id).toBeDefined();
         expect(user.name).toBe("Celio");
+
+        await expect(user.create({
+            userId: user.id,
+            title: "xxxx",
+            subject: "sub01",
+            content: "cont01",
+        })).resolves.toBeDefined();
+
+ await expect(News.create({})).rejects.toThrow();
+
     })
 });
 
--- frontend/src/pages/site/CardDetalhes.jsx
+++ frontend/src/pages/site/CardDetalhes.jsx
@@ -1,25 +1,61 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
-import { Breadcrumb, Button, Container } from 'react-bootstrap'
+import { Breadcrumb, Button, Col, Container, Row } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import { Link, useParams } from 'react-router-dom'
 import '../../assets/styles/CardDetalhes.css'
+import Footer from './components/Footer'
 
 export default function CardDetalhes() {
     const params = useParams();
-
     const [instituicao, setTnstituicao] = useState();
+    const [modalShow, setModalShow] = React.useState(false);
 
     useEffect(() => {
 
         fetch('http://localhost:3000/static/ong.json')
             .then((response) => response.json())
             .then(setTnstituicao);
-
     }, [params]);
+
+    function MyVerticallyCenteredModal(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+              Doa Bauru
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p><b><h2>
+              Compartilhe com seus amigose familia e ajude esse projeto a alcançar mais gente!
+              </h2></b></p>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="Text"
+                placeholder="Link Será aqui"
+                autoFocus
+              />
+            </Form.Group>
+            <Button>Copiar</Button>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
 
     if (!instituicao) return (<p>Carregando ...</p>)
 
     return (
+        <>
         <Container>
             <Breadcrumb>
                 <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Home</Breadcrumb.Item>
@@ -28,13 +64,37 @@ export default function CardDetalhes() {
                 </Breadcrumb.Item>
                 <Breadcrumb.Item active>{instituicao.name}</Breadcrumb.Item>
             </Breadcrumb>
-            <div className='imagem'>
-                <img src={instituicao.image} />
-            </div>
-            <div className='titulo'>
-                <h1>{instituicao.name}</h1>
-                <Button>Contato</Button>
-            </div>
-        </Container>
+
+    <Row>
+        <Col xs={12} md={8}>
+            <img className='imagem' src={instituicao.image} />
+        </Col>
+        <Col xs={6} md={4}>
+        <h2>{instituicao.name}</h2>
+        <h4>Sub-titulo aqui</h4>
+        <Button size="lg" onClick={() => window.open(instituicao.site)} href='javascript:void(0);'>Contato</Button>{' '}
+        <Button size="lg" variant="primary" onClick={() => setModalShow(true)}>Compartilhe</Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={12} md={5}>
+        <br/>
+          <b><h2>{instituicao.desc}</h2></b>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}><Button>Contato</Button></Col>
+        <Col xs={6}><Button variant="primary" onClick={() => setModalShow(true)}>Compartilhe</Button>
+        </Col>
+        <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </Row>
+      </Container>
+      <br/>
+      <Footer/>
+</>
     )
 }
