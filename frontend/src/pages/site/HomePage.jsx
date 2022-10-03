4edbd6d0bdbea6158f9bc26004a5ef069ed6e5ce
Erikpm16 <erikpm16@hotmail.com>
2022-10-03 20:25:29 -0300

    teste de cadastro

--- frontend/src/pages/site/HomePage.jsx
+++ frontend/src/pages/site/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navibar from './components/Navibar'
 import { Outlet } from "react-router-dom";
+import OngsContext from './context/OngsContext';
 
 export default function HomePage() {
   return (
     <div>
+        <OngsContext>
         <Navibar />
         <Outlet />
+        </OngsContext>
     </div>
   )
 }
